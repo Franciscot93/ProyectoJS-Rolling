84c1ccad3854849675493b202f18d49654f92a29
Franciscot93 <franciscot.p.93@gmail.com>
2022-09-13 21:17:05 -0300

    pequeñas modificaciones de validacion en el loguin

--- assets/js/validacion-login.js
+++ assets/js/validacion-login.js
@@ -23,24 +23,33 @@ if(emailLogin.value===''|| passwordLogin.value===''){
 } else if(!validarUserRegistrado.find(user=>user.correo===emailLogin.value)){
     mensajeLogin.classList.add('inputErrorDeFormulario-activo');
     mensajeLogin.classList.remove('msgLogin');
-    mensajeLogin.innerHTML=`<p>No existe una cuenta con esa cuenta de E-mail.</p>`
+    mensajeLogin.innerHTML=`<p>Validando datos ingresados.</p>`
+    setTimeout(()=>{mensajeLogin.innerHTML=`<p>No existe una cuenta con esa cuenta de E-mail.</p>`},2000) 
+    
 
 }else if((validarUserRegistrado.find(user=>user.correo===emailLogin.value).pass!== passwordLogin.value)){
     console.log(emailLogin.value);
     console.log(passwordLogin.value);
+    mensajeLogin.innerHTML=`<p>Validando datos ingresados.</p>`
 
     mensajeLogin.classList.add('inputErrorDeFormulario-activo');
     mensajeLogin.classList.remove('msgLogin');
-    mensajeLogin.innerHTML=`<p>La contraseña es incorrecta</p>`;
+    setTimeout(()=>{mensajeLogin.innerHTML=`<p>La contraseña es incorrecta</p>`;},2000) 
 
 }else if((validarUserRegistrado.find(user=>user.correo===emailLogin.value).pass=== passwordLogin.value)){
     mensajeLogin.classList.add('inputExitoDeFormulario-activo');
     mensajeLogin.classList.remove('msgLogin');
-    setTimeout(() => {
-        mensajeLogin.innerHTML=`<p>Validando datos ingresados.</p>`
-    }, 2000); 
     emailLogin.value='';
     passwordLogin.value='';
+    
+    mensajeLogin.innerHTML=`<p>Validando datos ingresados.</p>`
+    setTimeout(() => {
+        mensajeLogin.innerHTML=`<p>Conectando...</p>`
+    }, 2000); 
+    
+    setTimeout(() => {
+        window.location.href="/administracion.html"
+    }, 4000); 
     
 }
 
